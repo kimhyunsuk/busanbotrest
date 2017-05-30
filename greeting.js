6ffc351dad8d87122fc67b7b59fc2617bfd91912
erlking87 <kyojin77@lycos.co.kr>
2017-05-30 15:51:17 +0900

    commit

--- greeting.js
+++ greeting.js
@@ -135,7 +135,7 @@ exports.create = function (bot) {
                         .subtitle('Grandizer')
                         .text("안녕하세요. 저는 현대자동차의 그랜저 ig를 소개하는 그랜다이저예요. \n\n 대화중 언제든지'그랜다이저' 라고 입력하면 초기 화면으로 돌아가요. \n\n Hi. My name is Grandizer. \n\n At any time, type 'Grandizer' to return to the initial screen. ")
                         .images([
-                            builder.CardImage.create(session, "http://cfile9.uf.tistory.com/image/2577C9365521500608916E")
+                            builder.CardImage.create(session, "http://webbot02.azurewebsites.net/hyundai/images/img_car01.jpg")
                         ])
                         .buttons([
                             builder.CardAction.imBack(session, "한국어로 해줘", "한국어"),
