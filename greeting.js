dd3b1f53c33bf9b3bf16181fe99987c7bc9a7362
erlking87 <kyojin77@lycos.co.kr>
2017-05-30 15:17:14 +0900

    test commit

--- greeting.js
+++ greeting.js
@@ -126,7 +126,28 @@ exports.create = function (bot) {
     
 
     bot.dialog('start', [
-        function (session,args,next) {            
+        function (session,args,next) {
+            var msg = new builder.Message(session)
+                .attachments([
+                    { contentType: "application/json" },
+                    new builder.VideoCard(session)
+                        .title('그랜다이저')
+                        .autostart(true)
+                        .subtitle('Grandizer')
+                        .text("안녕하세요. 저는 현대자동차의 그랜저 ig를 소개하는 그랜다이저예요. \n\n 대화중 언제든지'그랜다이저' 라고 입력하면 초기 화면으로 돌아가요. \n\n Hi. My name is Grandizer. \n\n At any time, type 'Grandizer' to return to the initial screen. ")
+                        //.images([
+                        //    builder.CardImage.create(session, img_path + "/images/img_car01.jpg")
+                        //])
+                        .media([
+                            { url: 'https://youtu.be/VQtonf1fv_s?list=PLNSKpl7JCPswwj_fWkfixq5L7QeZJA4Ot' }
+                        ])
+                        .buttons([
+                            builder.CardAction.imBack(session, "한국어로 해줘", "한국어"),
+                            builder.CardAction.imBack(session, "hey there", "English")
+                        ])
+                ]);
+
+            session.send(msg);
             builder.Prompts.text(session, "챗봇을 시작하시려면 '시작' 하고 말씀해주세요.");
         },
         function (session, results, next) {
